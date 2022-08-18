28a73ca6c2d478937b85626d76e45570b00c08a1
Kevin2133 <kevin.bellocchio@gmail.com>
2022-08-18 14:48:02 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const serverless = require("serverless-http");
+const server = express();
+server.use(cors({
+    origin: '*'
+}));
+const data = require("./data.json");
+const router = express.Router();
+
+
+
+
+router.get("/", (req, res) => {
+    res.header("Content-Type", "application/json");
+    res.send(data);
+    res.end();
+});
+
+router.get("/manuel", (req, res) => {
+    res.header("Content-Type", "application/json");
+    res.send("Bella Elia");
+    res.end();
+});
+
+server.use("/.netlify/functions/api", router)
+
+module.exports.handler = serverless(server);
+
