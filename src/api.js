d303467c06492785301397217ab6ffb26603b55f
Kevin2133 <kevin.bellocchio@gmail.com>
2022-08-20 12:20:38 +0200

    prova per comunicare con api

--- src/api.js
+++ src/api.js
@@ -23,6 +23,12 @@ router.get("/manuel", (req, res) => {
     res.end();
 });
 
+router.get("https://kevinbellocchio.netlify.app/homepage", (req, res){
+    res.send("Content-Type", "application/json");
+    res.send("writing from api");
+    res.end();
+})
+
 server.use("/.netlify/functions/api", router);
 
 
